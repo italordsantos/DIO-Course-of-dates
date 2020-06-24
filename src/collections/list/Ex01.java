a399f1ae87c2c2019d2e8ef90853a7a5bac40d2c
Italo Rodrigues <italo.rgrego@gmail.com>
2020-06-24 11:09:18 -0300

    Criação da primeira atividade de Collections juntamente com a estrutura

---
+++ src/collections/list/Ex01.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections.list;
+
+/**
+ *
+ * @author italo
+ */
+public class Ex01 {
+    
+    /**
+     * Crie uma lista e execute os seguintes comandos
+     *  Adicione 5 nomes: Juliana, Larissa, Carlos, Pedro e João
+     *  Navegue na lista exibindo cada nome no console
+     *  Substitua o nome Carlos por Roberto
+     *  Retorne o nome da posição 1
+     *  Remova o nome da posição 4
+     *  Remova o nome João
+     *  Retorne a quantidade de nomes na lista
+     *  Crie uma lista com os nomes: Ismael e Rodrigo
+     *  Adicione todos os nomes da nova lista à primeira
+     *  Ordene os nomes da lista em ordem alfabética
+     *  Verifique se a lista está vazia
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+}
