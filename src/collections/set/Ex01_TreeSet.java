4892e24a9d2a73f1a68fef0e865e8dc8a40a0f5a
Italo Rodrigues <italo.rgrego@gmail.com>
2020-06-27 09:46:23 -0300

    Resolução do exercício de TreeSet

--- src/collections/set/Ex01_TreeSet.java
+++ src/collections/set/Ex01_TreeSet.java
@@ -5,6 +5,8 @@
  */
 package collections.set;
 
+import java.util.TreeSet;
+
 /**
  *
  * @author italo
@@ -23,6 +25,38 @@ public class Ex01_TreeSet {
      * @param args 
      */
     public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
         
+        System.out.println("->Adicione 5 números inteiros: 5, 88, 20, 44, 3");
+        System.out.println(treeSet);
+        treeSet.add(5);
+        treeSet.add(88);
+        treeSet.add(20);
+        treeSet.add(44);
+        treeSet.add(3);
+        System.out.println(treeSet);
+        System.out.println("");
+        
+        System.out.println("->Navegue no set exibindo cada número no console");
+        System.out.println(treeSet);
+        System.out.println("");
+        
+        System.out.println("->Remova o primeiro item do set");
+        System.out.println(treeSet);
+        treeSet.remove(treeSet.first());
+        System.out.println(treeSet);
+        System.out.println("");
+        
+        System.out.println("->Adicione um novo número no set: 23");
+        treeSet.add(23);
+        System.out.println(treeSet);
+        System.out.println("");
+        
+        System.out.println("->Verifique o tamanho do set");
+        System.out.println(treeSet.size());
+        System.out.println("");
+        
+        System.out.println("Verifique se o set está vazio");
+        System.out.println(treeSet.isEmpty());
     }
 }
