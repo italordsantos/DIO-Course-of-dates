c87b5ee02ae13c3ae4301800aa89ba23b946cfcb
Italo Rodrigues <italo.rgrego@gmail.com>
2020-06-27 09:00:04 -0300

    Criacao da estrutura de collection SET com exercicio

---
+++ src/collections/set/Ex01_LinkedSet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections.set;
+
+/**
+ *
+ * @author italo
+ */
+public class Ex01_LinkedSet {
+    
+    /**
+     * Crie um set e execute as seguintes operações
+     *  Adicione 5 números inteiros: 5, 88, 20, 44, 3
+     *  Navegue no set exibindo cada número no console
+     *  Remova o primeiro item do set
+     *  Adicione um novo número no set: 23
+     *  Verifique o tamanho do set
+     *  Verifique se o set está vazio
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
