7468af9e2b65a165a1b284930e60d8e92178fb5a
Italo Rodrigues <italo.rgrego@gmail.com>
2020-06-27 09:38:08 -0300

    Resolução do exercício de HashSet

--- src/collections/set/Ex01_HashSet.java
+++ src/collections/set/Ex01_HashSet.java
@@ -5,6 +5,8 @@
  */
 package collections.set;
 
+import java.util.HashSet;
+
 /**
  *
  * @author italo
@@ -13,16 +15,49 @@ public class Ex01_HashSet {
     
     /**
      * Crie um set e execute as seguintes operações
-     *  Adicione 5 números inteiros: 5, 88, 20, 44, 3
-     *  Navegue no set exibindo cada número no console
-     *  Remova o primeiro item do set
-     *  Adicione um novo número no set: 23
-     *  Verifique o tamanho do set
-     *  Verifique se o set está vazio
+     *      Adicione 5 números inteiros: 5, 88, 20, 44, 3
+     *      Navegue no set exibindo cada número no console
+     *      Remova o primeiro item do set
+     *      Adicione um novo número no set: 23
+     *      Verifique o tamanho do set
+     *      Verifique se o set está vazio
      * 
      * @param args 
      */
     public static void main(String[] args) {
         
+        HashSet<Integer> hashSet = new HashSet<>();
+        
+        System.out.println("->Adicione 5 números inteiros: 5, 88, 20, 44, 3");
+        System.out.println(hashSet);
+        hashSet.add(5);
+        hashSet.add(88);
+        hashSet.add(20);
+        hashSet.add(44);
+        hashSet.add(3);
+        System.out.println(hashSet);
+        System.out.println("");
+        
+        System.out.println("->Navegue no set exibindo cada número no console");
+        System.out.println(hashSet);
+        System.out.println("");
+        
+        System.out.println("->Remova o primeiro item do set");
+        System.out.println(hashSet);
+        hashSet.remove(3);
+        System.out.println(hashSet);
+        System.out.println("");
+        
+        System.out.println("->Adicione um novo número no set: 23");
+        hashSet.add(23);
+        System.out.println(hashSet);
+        System.out.println("");
+        
+        System.out.println("->Verifique o tamanho do set");
+        System.out.println(hashSet.size());
+        System.out.println("");
+        
+        System.out.println("Verifique se o set está vazio");
+        System.out.println(hashSet.isEmpty());
     }
 }
