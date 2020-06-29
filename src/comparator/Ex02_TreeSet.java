b1fd55174d8d390398b812faf92cb579f26cd067
Italo Rodrigues <italo.rgrego@gmail.com>
2020-06-29 14:24:51 -0300

    Criação da estrutura com exercícios para Comparator

---
+++ src/comparator/Ex02_TreeSet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparator;
+
+/**
+ *
+ * @author italo
+ */
+public class Ex02_TreeSet {
+    
+    /**
+     * Crie uma lista de um objeto complexo com as seguintes operações:
+     *  Adicione elementos nesta lsita
+     *  Ordene implementando a interface java.util.Compatator no seu objeto complexo
+     *  Ordene mplementando um novo objeto com a interface java.util.Comparable
+     *  Ordene usando uma expressão lambda na chamada de suaLista.sort()
+     *  Ordene usando referências de métodos e os métodos estátcos de Comparator
+     *  Ordene coleções TreeSet e TreeMap
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
