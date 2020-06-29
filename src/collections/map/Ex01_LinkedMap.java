5097d0c07a06d08dc2e4bb75b4021eb4121dcfbc
Italo Rodrigues <italo.rgrego@gmail.com>
2020-06-29 08:40:44 -0300

    Criação da estrutura de MAP e dos exercício com enunciado

---
+++ src/collections/map/Ex01_LinkedMap.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections.map;
+
+/**
+ *
+ * @author italo
+ */
+public class Ex01_LinkedMap {
+        
+    /**
+     * Crie um map e execute as seguintes operações:
+     *  Adicione os 26 estados Brasileiros no map, onde a sigla será a chave e o nome do estado o valor
+     *  Remova o estado de Minas Gerais
+     *  Adicione o Distrito Federal
+     *  Verifique o tamanho do mapa
+     *  Remova o estado do Mato Grosso do Sul usando o nome
+     *  Navegue em todos os registros do map, mostrando no console no seguinte formato: NOME (SIGLA)
+     *  Verifique se o estado de Santa Catarina existe no map buscando por sua sigla (SC)
+     *  Verifique se o estado de Maranhão existe no map buscando por seu nome
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
