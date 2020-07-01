9da1d1a023e39015789836b81440f89af91ce00f
Italo Rodrigues <italo.rgrego@gmail.com>
2020-07-01 10:44:38 -0300

    Criação da estrutura para Option e exercício

---
+++ src/collections/optional/Ex01_Optional.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections.optional;
+
+/**
+ *
+ * @author italo
+ */
+public class Ex01_Optional {
+    
+    /**
+     * Crie umn optional de um determinado tipo de dado
+     *  Crie com estado vazio, estado presente, e com null
+     *  Se presente, exiba o valor no console
+     *  Se vazio, lance uma exceção IllegalStateException
+     *  Se vazio, exiba "Optional vazio" no console
+     *  Se presente, transforme o valor e exiba no console
+     *  Se presente, pegue o valor do oprtional e atribua em uma variável
+     *  Se presente, filtre o optional com uma determinada regra de negócio
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
