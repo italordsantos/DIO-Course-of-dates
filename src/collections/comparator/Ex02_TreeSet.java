54c39949f23eda915ed9975d5631ed4e9886f1dd
Italo Rodrigues <italo.rgrego@gmail.com>
2020-07-01 09:32:00 -0300

    Resolução do Exercicio 2 de Comparator (treeSet)

--- src/collections/comparator/Ex02_TreeSet.java
+++ src/collections/comparator/Ex02_TreeSet.java
@@ -5,6 +5,12 @@
  */
 package collections.comparator;
 
+import collections.comparator.exemploaula.Estudante;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
 /**
  *
  * @author italo
@@ -13,9 +19,9 @@ public class Ex02_TreeSet {
     
     /**
      * Crie uma lista de um objeto complexo com as seguintes operações:
-     *  Adicione elementos nesta lsita
-     *  Ordene implementando a interface java.util.Compatator no seu objeto complexo
-     *  Ordene mplementando um novo objeto com a interface java.util.Comparable
+     *      Adicione elementos nesta lsita
+     *      Ordene implementando a interface java.util.Compataror no seu objeto complexo
+     *      Ordene implementando um novo objeto com a interface java.util.Comparable
      *  Ordene usando uma expressão lambda na chamada de suaLista.sort()
      *  Ordene usando referências de métodos e os métodos estátcos de Comparator
      *  Ordene coleções TreeSet e TreeMap
@@ -23,6 +29,22 @@ public class Ex02_TreeSet {
      * @param args 
      */
     public static void main(String[] args) {
+        TreeSet<Estudante> listaSemOrdenacao = new TreeSet<>(),
+                           listaOrdemNatural = new TreeSet<>(Comparator.comparingInt(Estudante::getIdade)),
+                           listaOrdemReversa = new TreeSet<>(Comparator.comparingInt(Estudante::getIdade).reversed());
+        
+        listaSemOrdenacao.add(new Estudante("Italo", 20));
+        listaSemOrdenacao.add(new Estudante("Karol", 19));
+        listaSemOrdenacao.add(new Estudante("Bili", 17));
+        listaSemOrdenacao.add(new Estudante("Auau", 15));
+        listaSemOrdenacao.add(new Estudante("Astoufo", 25));
+        
+        listaOrdemNatural.addAll(listaSemOrdenacao);
+        listaOrdemReversa.addAll(listaSemOrdenacao);
+        
+        System.out.println("->Ordene implementando a interface java.util.Compatator no seu objeto complexo");
+        System.out.println(listaOrdemNatural);
+        System.out.println(listaOrdemReversa);
         
     }
 }
